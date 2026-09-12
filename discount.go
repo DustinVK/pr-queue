b21c2884298d382f1414b9107dd8fee0587f43bc
Dustin VanKrimpen <Dustinrvk@gmail.com>
2026-09-12 03:52:48 -0400

    Use percentage factor for discounted totals

--- discount.go
+++ discount.go
@@ -3,5 +3,5 @@ package fixture
 // DiscountedTotal returns a price in cents after a percentage discount.
 // The discount is an integer from 0 through 100; fractional cents round down.
 func DiscountedTotal(cents, percent int) int {
-	return cents - cents*percent/100
+	return cents * (1 - percent/100)
 }
